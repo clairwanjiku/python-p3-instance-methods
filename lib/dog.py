da29dbfb8fe6f0ff1381a27886fc8d97aa54a5dd
Clair <clair.karanja@student.moringaschool.com>
2023-12-04 16:09:37 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 class Dog:
     def __init__(self, name):
         self.name = name
@@ -9,4 +7,3 @@ class Dog:
 
     def sit(self):
         print("The dog is sitting.")
-
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
+from lib.dog import Dog 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
